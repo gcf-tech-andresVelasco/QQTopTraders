b05c22b73fba8dd7fef4d345296e459bf35a7654
Andres Velasco <andresfelipecoronado@gmail.com>
2024-12-25 21:31:18 -0500

    Implemented the account selector

--- frontend/src/components/reactComponents/Overview.jsx
+++ frontend/src/components/reactComponents/Overview.jsx
@@ -4,6 +4,7 @@ import "./Overview.css";
 
 export default function Overview() {
   const [data, setData] = useState(null);
+  const [userSelected, setUserSelected] = useState(10003);
   useEffect(() => {
     fetch("https://spreads.gcf.group/fx/10003")
       // fetch('http://localhost:3000/fx/10003')
@@ -22,12 +23,32 @@ export default function Overview() {
     }).format(number);
     return formattedDollarNoDecimals;
   };
+  const fetchData = (user) => {
+    fetch(`https://spreads.gcf.group/fx/${user}`)
+      // fetch('http://localhost:3000/fx/10003')
+      .then((response) => response.json())
+      .then((json) => {
+        let obj = JSON.parse(json);
+        setData(obj);
+      });
+  }
+  const onChangeHandler = (e) => {
+    const selected = e.target.value
+    setUserSelected(selected)
+    fetchData(selected)
+  }
 
   return (
     <div className="grid-data">
       <div>
+      <select value={userSelected} onChange={(e) => onChangeHandler(e)}>
+        <option value={10003}>10003</option>
+        <option value={10002}>10002</option>
+        <option value={10021}>10021</option>
+        {/* <option value={2}>Equity</option> */}
+      </select>
         <div className="table-title">
-          <h3>10003</h3>
+          <h3>{userSelected}</h3>
           <p>USD</p>
         </div>
         <table>

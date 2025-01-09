a13070afcf53d16fc8c14e8c59baf4e929830183
Andres Velasco <andresfelipecoronado@gmail.com>
2025-01-09 17:44:28 -0500

    Implemented the new char with the values

--- frontend/src/components/reactComponents/CharSelector.jsx
+++ frontend/src/components/reactComponents/CharSelector.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import PrincipalChar from './PrincipalChar'
 import "./CharSelector.css"
+import BarChar from './BarChar'
 
 export default function CharSelector({data}) {
   const [selected, setSelected] = useState(0) // 0 = balance, 1 = profit, 2 = equity 
@@ -11,11 +12,13 @@ export default function CharSelector({data}) {
     <div>
       <select value={selected} onChange={(e) => onChangeHandler(e)}>
         <option value={0}>Balance</option>
-        <option value={1}>Profit</option>
+        <option value={1}>Profit Diario</option>
+        <option value={2}>Retorno Mensual</option>
         {/* <option value={2}>Equity</option> */}
       </select>
       {selected === 0 && <PrincipalChar data={data.balanceMonthly} />}
       {selected === 1 && <PrincipalChar data={data.profitMonthly} />}
+      {selected === 2 && <BarChar data={data.returnMonthly} />}
       
     </div>
   )
